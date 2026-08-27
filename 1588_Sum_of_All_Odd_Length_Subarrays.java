a8f862b79e0149fc83641591ef4441a97f6b8f47
Muthuveer76 <muthuveer124@gmail.com>
2026-08-27 18:39:15 +0530

    Auto-commit: 1588_Sum_of_All_Odd_Length_Subarrays

---
+++ 1588_Sum_of_All_Odd_Length_Subarrays.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int sumOddLengthSubarrays(int[] arr) {
+        int res = 0;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i; j < arr.length; j += 2) {
+                for (int k = i; k <= j; k++) {
+                    res += arr[k];
+                }
+            }
+        }
+        return res;
+    }
+}
