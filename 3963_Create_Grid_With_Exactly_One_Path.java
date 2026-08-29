f766b81387c8b6baa08abcede2d453b232aabe62
Muthuveer76 <muthuveer124@gmail.com>
2026-08-29 22:34:19 +0530

    Auto-commit: 3963_Create_Grid_With_Exactly_One_Path

---
+++ 3963_Create_Grid_With_Exactly_One_Path.java
@@ -0,0 +1,25 @@
+class Solution {
+    public String[] createGrid(int m, int n) {
+        char[][] grid = new char[m][n];
+
+        for (int i = 0; i < m; i++) {
+            Arrays.fill(grid[i], '#');
+        }
+
+        for (int j = 0; j < n; j++) {
+            grid[0][j] = '.';
+        }
+
+        for (int i = 0; i < m; i++) {
+            grid[i][n - 1] = '.';
+        }
+
+        String[] ans = new String[m];
+
+        for (int i = 0; i < m; i++) {
+            ans[i] = new String(grid[i]);
+        }
+
+        return ans;
+    }
+}
