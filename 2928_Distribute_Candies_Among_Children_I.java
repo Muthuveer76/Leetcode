98ccde3556be802e3345dc0fd764841d7702f287
Muthuveer76 <muthuveer124@gmail.com>
2026-08-29 22:26:55 +0530

    Auto-commit: 2928_Distribute_Candies_Among_Children_I

---
+++ 2928_Distribute_Candies_Among_Children_I.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int distributeCandies(int n, int limit) {
+        int ans = 0;
+        for (int i = 0; i <= Math.min(n, limit); i++) {
+            for (int j = 0; j <= Math.min(n - i, limit); j++) {
+                int k = n - i - j;
+
+                if (k >= 0 && k <= limit) {
+                    ans++;
+                }
+            }
+        }
+        return ans;
+    }
+}
