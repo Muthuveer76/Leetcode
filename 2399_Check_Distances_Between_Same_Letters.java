b86fea13d8c5cb596dd26ca198cd0ce073773de9
Muthuveer76 <muthuveer124@gmail.com>
2026-08-30 23:44:30 +0530

    Auto-commit: 2399_Check_Distances_Between_Same_Letters

---
+++ 2399_Check_Distances_Between_Same_Letters.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean checkDistances(String s, int[] distance) {
+        int[] first = new int[26];
+        Arrays.fill(first, -1);
+        for (int i = 0; i < s.length(); i++) {
+            int index = s.charAt(i) - 'a';
+
+            if (first[index] == -1) {
+                first[index] = i;
+            } else {
+                if (i - first[index] - 1 != distance[index]) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
