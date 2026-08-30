a067753233ff7e234739b7a9742414fd7a342f6e
Muthuveer76 <muthuveer124@gmail.com>
2026-08-30 23:10:38 +0530

    Auto-commit: 908_Smallest_Range_I

---
+++ 908_Smallest_Range_I.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int smallestRangeI(int[] nums, int k) {
+        int min=Integer.MAX_VALUE,max=Integer.MIN_VALUE;
+        for(int x:nums){
+            min=Math.min(min,x);
+            max=Math.max(max,x);
+        }
+        return Math.max(0,max-min-2*k);
+    }
+}
