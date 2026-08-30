832bf1921df49e0f073b7bbe5af0ec12cd910633
Muthuveer76 <muthuveer124@gmail.com>
2026-08-30 23:09:38 +0530

    Auto-commit: 1403_Minimum_Subsequence_in_Non_Increasing_Order

---
+++ 1403_Minimum_Subsequence_in_Non_Increasing_Order.java
@@ -0,0 +1,14 @@
+class Solution {
+    public List<Integer> minSubsequence(int[] nums) {
+        Arrays.sort(nums);
+        int total=0,sum=0;
+        for(int x:nums) total+=x;
+        List<Integer> ans=new ArrayList<>();
+        for(int i=nums.length-1;i>=0;i--){
+            sum+=nums[i];
+            ans.add(nums[i]);
+            if(sum>total-sum) break;
+        }
+        return ans;
+    }
+}
