3623c9515322bc5edf979a1f419a4c567aba416c
Muthuveer76 <muthuveer124@gmail.com>
2026-08-31 23:33:31 +0530

    Auto-commit: 807_Max_Increase_to_Keep_City_Skyline

---
+++ 807_Max_Increase_to_Keep_City_Skyline.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int maxIncreaseKeepingSkyline(int[][] grid) {
+        int n = grid.length;
+        int m = grid[0].length;
+        int[] row = new int[n];
+        int[] col = new int[m];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                row[i] = Math.max(row[i], grid[i][j]);
+                col[j] = Math.max(col[j], grid[i][j]);
+            }
+        }
+
+        int ans = 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                ans += Math.min(row[i], col[j]) - grid[i][j];
+            }
+        }
+
+        return ans;
+    }
+}
