d9f728dac715cb33ac249d8666d6fbdf9ed81d0e
Muthuveer76 <muthuveer124@gmail.com>
2026-09-01 11:58:48 +0530

    Auto-commit: 1630_Arithmetic_Subarrays

---
+++ 1630_Arithmetic_Subarrays.java
@@ -0,0 +1,24 @@
+class Solution {
+    public List<Boolean> checkArithmeticSubarrays(int[] nums, int[] l, int[] r) {
+        List<Boolean> ans = new ArrayList<>();
+
+        for (int i = 0; i < l.length; i++) {
+            int[] arr = Arrays.copyOfRange(nums, l[i], r[i] + 1);
+            Arrays.sort(arr);
+
+            int diff = arr[1] - arr[0];
+            boolean valid = true;
+
+            for (int j = 2; j < arr.length; j++) {
+                if (arr[j] - arr[j - 1] != diff) {
+                    valid = false;
+                    break;
+                }
+            }
+
+            ans.add(valid);
+        }
+
+        return ans;
+    }
+}
