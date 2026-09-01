132a2b9f270d394478edc6f6a015d9d84940e15c
Muthuveer76 <muthuveer124@gmail.com>
2026-09-01 13:51:23 +0530

    Auto-commit: 1605_Find_Valid_Matrix_Given_Row_and_Column_Sums

---
+++ 1605_Find_Valid_Matrix_Given_Row_and_Column_Sums.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int[][] restoreMatrix(int[] rowSum, int[] colSum) {
+        int m = rowSum.length;
+        int n = colSum.length;
+        int[][] matrix = new int[m][n];
+
+        int i = 0;
+        int j = 0;
+
+        while (i < m && j < n) {
+            int value = Math.min(rowSum[i], colSum[j]);
+
+            matrix[i][j] = value;
+            rowSum[i] -= value;
+            colSum[j] -= value;
+
+            if (rowSum[i] == 0) {
+                i++;
+            }
+
+            if (colSum[j] == 0) {
+                j++;
+            }
+        }
+
+        return matrix;
+    }
+}
