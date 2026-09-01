5f4cde86f8cc1ff554b424679439d3bb93cc32ac
Muthuveer76 <muthuveer124@gmail.com>
2026-09-01 12:07:07 +0530

    Auto-commit: 3016_Minimum_Number_of_Pushes_to_Type_Word_II

---
+++ 3016_Minimum_Number_of_Pushes_to_Type_Word_II.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int minimumPushes(String word) {
+        int[] freq = new int[26];
+
+        for (char c : word.toCharArray()) {
+            freq[c - 'a']++;
+        }
+
+        Arrays.sort(freq);
+
+        int pushes = 0;
+
+        for (int i = 25; i >= 0; i--) {
+            int rank = 25 - i;
+            int cost = rank / 8 + 1;
+            pushes += freq[i] * cost;
+        }
+
+        return pushes;
+    }
+}
