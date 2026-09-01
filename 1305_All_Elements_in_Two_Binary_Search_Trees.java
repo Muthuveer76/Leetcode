08a3d16da751a973c274a6a582828f19ba7d02dd
Muthuveer76 <muthuveer124@gmail.com>
2026-09-01 14:36:54 +0530

    Auto-commit: 1305_All_Elements_in_Two_Binary_Search_Trees

---
+++ 1305_All_Elements_in_Two_Binary_Search_Trees.java
@@ -0,0 +1,53 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> getAllElements(TreeNode root1, TreeNode root2) {
+        List<Integer> a = new ArrayList<>();
+        List<Integer> b = new ArrayList<>();
+
+        inorder(root1, a);
+        inorder(root2, b);
+
+        List<Integer> result = new ArrayList<>();
+        int i = 0, j = 0;
+
+        while (i < a.size() && j < b.size()) {
+            if (a.get(i) <= b.get(j)) {
+                result.add(a.get(i++));
+            } else {
+                result.add(b.get(j++));
+            }
+        }
+
+        while (i < a.size()) {
+            result.add(a.get(i++));
+        }
+
+        while (j < b.size()) {
+            result.add(b.get(j++));
+        }
+
+        return result;
+    }
+
+    private void inorder(TreeNode root, List<Integer> list) {
+        if (root == null) return;
+
+        inorder(root.left, list);
+        list.add(root.val);
+        inorder(root.right, list);
+    }
+}
