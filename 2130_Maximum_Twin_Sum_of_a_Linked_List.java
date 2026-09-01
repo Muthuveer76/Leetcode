a8ce4a95083245f1bd129bb62a9f4c5679ba1df0
Muthuveer76 <muthuveer124@gmail.com>
2026-09-01 12:01:57 +0530

    Auto-commit: 2130_Maximum_Twin_Sum_of_a_Linked_List

---
+++ 2130_Maximum_Twin_Sum_of_a_Linked_List.java
@@ -0,0 +1,45 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public int pairSum(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        ListNode second = reverse(slow);
+        int ans = 0;
+
+        while (second != null) {
+            ans = Math.max(ans, head.val + second.val);
+            head = head.next;
+            second = second.next;
+        }
+
+        return ans;
+    }
+
+    private ListNode reverse(ListNode head) {
+        ListNode prev = null;
+
+        while (head != null) {
+            ListNode next = head.next;
+            head.next = prev;
+            prev = head;
+            head = next;
+        }
+
+        return prev;
+    }
+}
