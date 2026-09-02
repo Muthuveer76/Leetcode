104fae3faec1c085611351340a48c7279bf3458c
Muthuveer76 <muthuveer124@gmail.com>
2026-09-02 15:11:47 +0530

    Auto-commit: 1026_Maximum_Difference_Between_Node_and_Ancestor

---
+++ 1026_Maximum_Difference_Between_Node_and_Ancestor.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int maxAncestorDiff(TreeNode root) {
+        return dfs(root, root.val, root.val);
+    }
+
+    private int dfs(TreeNode node, int min, int max) {
+        if (node == null) {
+            return max - min;
+        }
+
+        min = Math.min(min, node.val);
+        max = Math.max(max, node.val);
+
+        return Math.max(dfs(node.left, min, max), dfs(node.right, min, max));
+    }
+}
