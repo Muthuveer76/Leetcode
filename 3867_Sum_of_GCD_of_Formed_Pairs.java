19eeb0ef9c15246c1518eb4f23d098b00f160dba
Muthuveer76 <muthuveer124@gmail.com>
2026-09-05 23:34:59 +0530

    Auto-commit: 3867_Sum_of_GCD_of_Formed_Pairs

---
+++ 3867_Sum_of_GCD_of_Formed_Pairs.java
@@ -0,0 +1,32 @@
+class Solution {
+    public long gcdSum(int[] nums) {
+        int n = nums.length;
+        int[] prefixGcd = new int[n];
+
+        int max = 0;
+
+        for (int i = 0; i < n; i++) {
+            max = Math.max(max, nums[i]);
+            prefixGcd[i] = gcd(nums[i], max);
+        }
+
+        Arrays.sort(prefixGcd);
+
+        long sum = 0;
+
+        for (int i = 0; i < n / 2; i++) {
+            sum += gcd(prefixGcd[i], prefixGcd[n - 1 - i]);
+        }
+
+        return sum;
+    }
+
+    private int gcd(int a, int b) {
+        while (b != 0) {
+            int temp = a % b;
+            a = b;
+            b = temp;
+        }
+        return a;
+    }
+}
