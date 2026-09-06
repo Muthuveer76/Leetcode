fac0f0f4c6d9e92ecfaef978d7250a4b696b8de6
Muthuveer76 <muthuveer124@gmail.com>
2026-09-06 20:10:42 +0530

    Auto-commit: 929_Unique_Email_Addresses

---
+++ 929_Unique_Email_Addresses.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int numUniqueEmails(String[] emails) {
+        Set<String> set = new HashSet<>();
+
+        for (String email : emails) {
+            String[] parts = email.split("@");
+            String local = parts[0].split("\\+")[0].replace(".", "");
+            set.add(local + "@" + parts[1]);
+        }
+
+        return set.size();
+    }
+}
