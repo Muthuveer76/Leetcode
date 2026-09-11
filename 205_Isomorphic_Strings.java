854528ecee6ea89b58270965942f46698fb07b51
Muthuveer76 <muthuveer124@gmail.com>
2026-09-11 10:42:58 +0530

    Auto-commit: 205_Isomorphic_Strings

--- 205_Isomorphic_Strings.java
+++ 205_Isomorphic_Strings.java
@@ -1,20 +1,22 @@
 class Solution {
     public boolean isIsomorphic(String s, String t) {
-        int[] mapS = new int[256];
-        int[] mapT = new int[256];
+        HashMap<Character, Integer> map = new HashMap<>();
+        HashMap<Character, Integer> map2 = new HashMap<>();
 
         for (int i = 0; i < s.length(); i++) {
-            char a = s.charAt(i);
-            char b = t.charAt(i);
-
-            if (mapS[a] != mapT[b]) {
-                return false;
+            if (!map.containsKey(s.charAt(i))) {
+                map.put(s.charAt(i), i);
             }
 
-            mapS[a] = i + 1;
-            mapT[b] = i + 1;
+            if (!map2.containsKey(t.charAt(i))) {
+                map2.put(t.charAt(i), i);
+            }
+
+            if(!map.get(s.charAt(i)).equals(map2.get(t.charAt(i)))){
+                return false;
+            }
         }
 
-        return true;
+        return true;        
     }
 }
