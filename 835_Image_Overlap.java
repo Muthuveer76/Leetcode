9f437fc679cc4a7deeeaee562fb243134b4145f3
Muthuveer76 <muthuveer124@gmail.com>
2026-09-13 22:12:57 +0530

    Auto-commit: 835_Image_Overlap

---
+++ 835_Image_Overlap.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int largestOverlap(int[][] img1, int[][] img2) {
+        int n = img1.length;
+        int max = 0;
+
+        for (int dr = -n + 1; dr < n; dr++) {
+            for (int dc = -n + 1; dc < n; dc++) {
+                int count = 0;
+                for (int i = 0; i < n; i++) {
+                    for (int j = 0; j < n; j++) {
+                        int r = i + dr;
+                        int c = j + dc;
+
+                        if (r >= 0 && r < n && c >= 0 && c < n) {
+                            if (img1[i][j] == 1 && img2[r][c] == 1) {
+                                count++;
+                            }
+                        }
+                    }
+                }
+                max = Math.max(max, count);
+            }
+        }
+        return max;
+    }
+}
